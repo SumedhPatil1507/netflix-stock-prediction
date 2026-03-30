b241725d4a4c93807b17bbac143c7cbada73f8d5
Sumedh Patil <manishapatil2312@gmail.com>
2026-03-30 16:48:46 +0530

    Added Streamlit UI

---
+++ app/app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import joblib
+import pandas as pd
+
+# Load model
+model = joblib.load("models/model.pkl")
+
+st.title("📈 Netflix Stock Prediction App")
+
+st.write("Enter stock details")
+
+# Inputs (MATCH TRAINING FEATURES)
+open_price = st.number_input("Open Price")
+high_price = st.number_input("High Price")
+low_price = st.number_input("Low Price")
+close_price = st.number_input("Close Price")
+volume = st.number_input("Volume")
+
+ma7 = st.number_input("MA7")
+ma21 = st.number_input("MA21")
+ret = st.number_input("Return")
+
+# Create dataframe EXACTLY same as training
+features = pd.DataFrame([[open_price, high_price, low_price, close_price, volume, ma7, ma21, ret]],
+                        columns=['Open','High','Low','Close','Volume','MA7','MA21','Return'])
+
+# Predict
+if st.button("Predict"):
+    prediction = model.predict(features)[0]
+    st.success(f"📈 Predicted Next Value: {prediction:.2f}")
